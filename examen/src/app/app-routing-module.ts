78fffc9d0d0d526cc4e959f27f01f6ed58bcdc85
Laura <lagonzalez@birt.eus>
2026-01-19 20:37:48 +0100

    CReado enrutamiento

--- examen/src/app/app-routing-module.ts
+++ examen/src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
+
+const routes: Routes = [
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/create', component: PostCreateComponent },
+  { path: 'posts/edit/:id', component: PostEditComponent },
+  { path: 'posts/:id', component: PostDetailComponent },
+  { path: '', redirectTo: '/posts', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
